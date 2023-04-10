793a7df2096c199761565bb81c0cb22d7cc30cef
scofield622 <rane09061@gmail.com>
2023-04-11 03:09:38 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <><h1>Woongjin's page</h1><h2>My hobby</h2><ul>
+            <li>playing drum</li>
+            <li>playing game</li>
+            <li>riding a bike</li>
+            <li>exercising with friends</li>
+        </ul>
+        <h2>What I want to do or learn about</h2><ol>
+            <li>Learn to speak English</li>
+            <li>Learn how to play go</li>
+            <li>Reading books</li></ol>
+            <p>The reason I want to learn conversational English is that I can listen and read, but I feel frustrated that I cannot speak or write directly.</p>
+            <p>The reason why I want to learn Go is that I learned it when I was young, but it is regrettable that I did not learn it deeply, and I want to try it with my grandfather later.</p></>)
+}
